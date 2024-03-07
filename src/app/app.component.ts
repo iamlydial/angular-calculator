c6c615062cf58331b917779047126f0029f306ab
iamlydial <lydia.lavecchia@gmail.com>
2024-03-07 20:39:15 +0100

    Click functionality
    added click functionality to the buttons with attribute wheret is a number or a function. And created the click function for the number.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,25 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'angular-calculator';
 
-  
+  calValue : number = 0;
+  funcT:any = 'No Function';
+
+  calNumber : string = 'no value';
+
+  onClickValue(val: string, type: any){
+    console.log(val, type);
+    if(type == 'number'){
+      this.onNumberClick(val);
+    }
+  }
+
+  onNumberClick(val : string){
+    if(this.calNumber !== 'novalue'){
+      this.calNumber = this.calValue + val;
+    } else {
+      this.calNumber = val;
+    }
+
+    this.calValue = parseFloat(this.calNumber);
+  }
 }
