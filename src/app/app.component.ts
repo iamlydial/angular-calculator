a785bdc20d25046b95768ee0d03e5b122242aede
iamlydial <lydia.lavecchia@gmail.com>
2024-03-07 21:25:54 +0100

    Added 5 function
    for the addition, subtraction, multiplication and division

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,30 +3,63 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'angular-calculator';
 
-  calValue : number = 0;
-  funcT:any = 'No Function';
+  calValue: number = 0;
+  funcT: any = 'NoFunction';
 
-  calNumber : string = 'no value';
+  calNumber: string = 'no value';
 
-  onClickValue(val: string, type: any){
+  firstNumber: number = 0;
+  secondNumber: number = 0;
+
+  onClickValue(val: string, type: any) {
     console.log(val, type);
-    if(type == 'number'){
+    if (type == 'number') {
       this.onNumberClick(val);
+    } else if (type == 'function') {
+      this.onFunctionClick(val);
     }
   }
 
-  onNumberClick(val : string){
-    if(this.calNumber !== 'novalue'){
+  onNumberClick(val: string) {
+    if (this.calNumber !== 'novalue') {
       this.calNumber = this.calValue + val;
     } else {
       this.calNumber = val;
     }
 
     this.calValue = parseFloat(this.calNumber);
+  }
+
+  onFunctionClick(val: string) {
+    if (this.funcT == 'NoFunction') {
+      this.firstNumber = this.calValue;
+      this.calValue = 0;
+      this.calNumber = 'noValue';
+      this.funcT = val;
+    } else if(this.funcT !== 'NoFunction'){
+      this.secondNumber = this.calValue;
+      this.valueCalculate(val);
+    }
+  }
+
+  valueCalculate(val: string){
+    if(this.funcT == '+'){
+      const Total = this.firstNumber + this.secondNumber;
+      this.calValue = Total;
+    } else if(this.funcT == '-'){
+      const Total = this.firstNumber - this.secondNumber;
+      this.calValue = Total;
+    } else if(this.funcT == '/'){
+      const Total = this.firstNumber / this.secondNumber;
+      this.calValue = Total;
+    } else if(this.funcT == '*'){
+      const Total = this.firstNumber * this.secondNumber;
+      this.calValue = Total;
+    }
   }
 }
