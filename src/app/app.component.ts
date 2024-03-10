1c1f8d98511d027806cfa9591a84f2f51a9d8968
iamlydial <lydia.lavecchia@gmail.com>
2024-03-10 11:48:20 +0100

    Added Calculator methods
    for the different operations and to rset it. Plus deployed it on Vercel
    
    https://angular-calculator-six.vercel.app/

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,7 @@ export class AppComponent {
   calValue: number = 0;
   funcT: any = 'NoFunction';
 
-  calNumber: string = 'no value';
+  calNumber: string = 'NoValue';
 
   firstNumber: number = 0;
   secondNumber: number = 0;
@@ -26,8 +26,8 @@ export class AppComponent {
   }
 
   onNumberClick(val: string) {
-    if (this.calNumber !== 'novalue') {
-      this.calNumber = this.calValue + val;
+    if (this.calNumber !== 'NoValue') {
+      this.calNumber = this.calNumber + val;
     } else {
       this.calNumber = val;
     }
@@ -36,30 +36,66 @@ export class AppComponent {
   }
 
   onFunctionClick(val: string) {
-    if (this.funcT == 'NoFunction') {
+    // call the clear all method when C button is clicked
+    if (val == 'c') {
+      this.clearAll();
+    } else if (this.funcT == 'NoFunction') {
       this.firstNumber = this.calValue;
       this.calValue = 0;
-      this.calNumber = 'noValue';
+      this.calNumber = 'NoValue';
       this.funcT = val;
-    } else if(this.funcT !== 'NoFunction'){
+    } else if (this.funcT !== 'NoFunction') {
       this.secondNumber = this.calValue;
       this.valueCalculate(val);
     }
   }
 
-  valueCalculate(val: string){
-    if(this.funcT == '+'){
+  valueCalculate(val: string) {
+    if (this.funcT == '+') {
       const Total = this.firstNumber + this.secondNumber;
-      this.calValue = Total;
-    } else if(this.funcT == '-'){
-      const Total = this.firstNumber - this.secondNumber;
-      this.calValue = Total;
-    } else if(this.funcT == '/'){
-      const Total = this.firstNumber / this.secondNumber;
-      this.calValue = Total;
-    } else if(this.funcT == '*'){
-      const Total = this.firstNumber * this.secondNumber;
-      this.calValue = Total;
+      this.totalAssignValues(Total, val);
     }
+    if (this.funcT == '-') {
+      const Total = this.firstNumber - this.secondNumber;
+      this.totalAssignValues(Total, val);
+    }
+    if (this.funcT == '*') {
+      const Total = this.firstNumber * this.secondNumber;
+      this.totalAssignValues(Total, val);
+    }
+    if (this.funcT == '/') {
+      const Total = this.firstNumber * this.secondNumber;
+      this.totalAssignValues(Total, val);
+    }
+    if (this.funcT == '%') {
+      const Total = this.firstNumber * this.secondNumber;
+      this.totalAssignValues(Total, val);
+    }
+  }
+
+  totalAssignValues(Total: number, val: string) {
+    this.calValue = Total;
+    this.firstNumber = Total;
+    this.secondNumber = 0;
+    this.calNumber = 'NoValue';
+    this.funcT = val;
+    if (val == '=') {
+      this.onEqualPress();
+    }
+  }
+
+  onEqualPress() {
+    this.firstNumber = 0;
+    this.secondNumber = 0;
+    this.funcT = 'NoFunction';
+    this.calNumber = 'NoValue';
+  }
+
+  clearAll() {
+    this.firstNumber = 0;
+    this.secondNumber = 0;
+    this.calValue = 0;
+    this.funcT = 'NoFunction';
+    this.calNumber = 'NoValue';
   }
 }
