a9ed1839f4fd71f40ed8182edb969f41adcd4395
iamlydial <lydia.lavecchia@gmail.com>
2024-03-10 20:29:45 +0100

    Revert "Last commit"
    This reverts commit 5b5ca62ae955ecc20debcd5ef006963e4dfb0c45.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,99 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  //styleUrl: './app.component.css',
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  
+  title = 'angular-calculator';
+
+  calValue: number = 0;
+  funcT: any = 'NoFunction';
+
+  calNumber: string = 'NoValue';
+
+  firstNumber: number = 0;
+  secondNumber: number = 0;
+
+  onClickValue(val: string, type: any) {
+    console.log(val, type);
+    if (type == 'number') {
+      this.onNumberClick(val);
+    } else if (type == 'function') {
+      this.onFunctionClick(val);
+    }
+  }
+
+  onNumberClick(val: string) {
+    if (this.calNumber !== 'NoValue') {
+      this.calNumber = this.calNumber + val;
+    } else {
+      this.calNumber = val;
+    }
+
+    this.calValue = parseFloat(this.calNumber);
+  }
+
+  onFunctionClick(val: string) {
+    // call the clear all method when C button is clicked
+    if (val == 'c') {
+      this.clearAll();
+    } else if (this.funcT == 'NoFunction') {
+      this.firstNumber = this.calValue;
+      this.calValue = 0;
+      this.calNumber = 'NoValue';
+      this.funcT = val;
+    } else if (this.funcT !== 'NoFunction') {
+      this.secondNumber = this.calValue;
+      this.valueCalculate(val);
+    }
+  }
+
+  valueCalculate(val: string) {
+    if (this.funcT == '+') {
+      const Total = this.firstNumber + this.secondNumber;
+      this.totalAssignValues(Total, val);
+    }
+    if (this.funcT == '-') {
+      const Total = this.firstNumber - this.secondNumber;
+      this.totalAssignValues(Total, val);
+    }
+    if (this.funcT == '*') {
+      const Total = this.firstNumber * this.secondNumber;
+      this.totalAssignValues(Total, val);
+    }
+    if (this.funcT == '/') {
+      const Total = this.firstNumber * this.secondNumber;
+      this.totalAssignValues(Total, val);
+    }
+    if (this.funcT == '%') {
+      const Total = this.firstNumber * this.secondNumber;
+      this.totalAssignValues(Total, val);
+    }
+  }
+
+  totalAssignValues(Total: number, val: string) {
+    this.calValue = Total;
+    this.firstNumber = Total;
+    this.secondNumber = 0;
+    this.calNumber = 'NoValue';
+    this.funcT = val;
+    if (val == '=') {
+      this.onEqualPress();
+    }
+  }
+
+  onEqualPress() {
+    this.firstNumber = 0;
+    this.secondNumber = 0;
+    this.funcT = 'NoFunction';
+    this.calNumber = 'NoValue';
+  }
+
+  clearAll() {
+    this.firstNumber = 0;
+    this.secondNumber = 0;
+    this.calValue = 0;
+    this.funcT = 'NoFunction';
+    this.calNumber = 'NoValue';
+  }
 }
